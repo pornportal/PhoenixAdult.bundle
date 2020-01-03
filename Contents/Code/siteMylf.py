814548bfde201a4a936df640a44203a40a893a4f
DirtyRacer1337 <DirtyRacer@ya.ru>
2020-01-03 07:35:10 +0700

    Fix search

--- Contents/Code/siteMylf.py
+++ Contents/Code/siteMylf.py
@@ -5,7 +5,7 @@ import PAactors
 def search(results,encodedTitle,title,searchTitle,siteNum,lang,searchByDateActor,searchDate,searchSiteID):
     if searchSiteID != 9999:
         siteNum = searchSiteID
-    searchString = searchTitle.replace(" ","-").replace(",","").replace("'","").replace("?","")
+    searchString = searchTitle.lower().replace(" ","-").replace(",","").replace("'","").replace("?","")
     Log("searchString: " + searchString)
     if "/" not in searchString:
         searchString = searchString.replace("-","/",1)
